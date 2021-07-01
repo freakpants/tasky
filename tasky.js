42c8507d7b7d41eedab6295fd12e6199a2145498
Christian Nyffenegger <freakpants@gmail.com>
2021-07-01 12:51:01 +0200

    Create tasky.js

---
+++ tasky.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Tasky
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://odoo.simpit.ch/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert('tasky running');
+    // Your code here...
+})();
